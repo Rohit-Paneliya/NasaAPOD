993c5bac83430ea9db0bcf25dcc5d7104cb1c143
Rohit Paneliya <rohit.paneliya@joshsoftware.com>
2022-08-15 09:07:30 +0530

    Splash screen added with Android 12 support

--- app/src/main/java/com/nasa/apod/presentation/main/MainActivity.kt
+++ app/src/main/java/com/nasa/apod/presentation/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.nasa.apod.presentation.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
@@ -18,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
